655484cc5d3ae3b59d0350f169972d3fd9b2a5d3
ngostradamus <roninvideostudio@gmail.com>
2020-11-22 11:41:18 -0800

    added If statement logic and Symbol parameter for lookup

--- apicall.py
+++ apicall.py
@@ -7,11 +7,11 @@ import json
 
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 parameters = {
-    'id':'1'
+    'symbol':'ETH'
 }
 headers = {
   'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': 'paste API key here',
+  'X-CMC_PRO_API_KEY': '',
 }
 
 session = Session()
@@ -20,7 +20,18 @@ session.headers.update(headers)
 try:
   response = session.get(url, params=parameters)
   data = json.loads(response.text)
-  print(data)
+  #print(data)
+  parseData = json.dumps(response.json())
+  #print(parseData)
+  ethObj = json.loads(parseData)
+  print(ethObj["data"]["ETH"]["name"])
+  print(ethObj["data"]["ETH"]["quote"]["USD"]["price"])
+
+  ethString = str(ethObj["data"]["ETH"]["quote"]["USD"]["price"])
+  ethPrice = float(ethString)
+
+  if(ethPrice > 500):
+    print("I'm filthy rich!")
 except (ConnectionError, Timeout, TooManyRedirects) as e:
   print(e)
   
